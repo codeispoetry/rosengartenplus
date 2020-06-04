25456cc743d6470d5d32f732628e0f6e04f7fdfd
Thomas Rose <mail@tom-rose.de>
2020-06-05 00:24:19 +0200

    add tests

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('https://sharepicgenerator.de/wordpress');
+        await driver.wait(until.titleIs('Sharepicgenerator'), 1000);
+        console.log("passed");
+    } catch( error ){
+        const core = require('@actions/core');
+        core.setFailed(error.message);
+    } finally {
+        await driver.quit();
+    }
+})();
